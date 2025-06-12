22b84ebaa52416e17a33e8b66e75a923efeaae1d
jeevanmv <jeevanmv1998@gmail.com>
2025-06-12 14:53:47 +0530

    first commit

---
+++ Bluetooth-Features-main/run.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.run("python constants.py", shell=True)
+subprocess.run("python gap.py", shell=True)
+subprocess.run("python logger_mod.py", shell=True)
+subprocess.run("python trigger.py", shell=True)
+subprocess.run("python utils.py", shell=True)
